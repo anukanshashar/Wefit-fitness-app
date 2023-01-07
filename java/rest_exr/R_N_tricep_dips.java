01941bcc2a18dade9789e97aaf9a54586d02bc45
anukanshashar <75297454+anukanshashar@users.noreply.github.com>
2023-01-07 14:04:21 +0530

    tricep dips java file. ill handle the rest now. thanks!

---
+++ java/rest_exr/R_N_tricep_dips.java
@@ -0,0 +1,95 @@
+package com.example.fitnessapp.rest_exc;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class R_N_tricep_dips extends AppCompatActivity {
+    private int seconds = 15;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.exersice_tricep_2);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Tricep Dips");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Sit on a chair or a bench with your hands on the seat beside you and your feet flat on the floor.\n" +
+                "2. Lift your butt off the chair and from this position, lower your body toward the ground until your arms are bent at 90-degree angles.\n" +
+                "3. Push yourself back up to the starting position and repeat.\n" +
+                "4. To make it more difficult, do this with your legs extended straight with your heels on the floor and toes pointed up, making sure to keep your butt off the ground.\n" +
+                "5. For even more of a challenge, place both feet on another bench.");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
