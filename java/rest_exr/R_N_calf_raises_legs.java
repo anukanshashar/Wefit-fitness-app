849179186240b3895bf3785a4aa59fc80fb9aa0f
anukanshashar <75297454+anukanshashar@users.noreply.github.com>
2023-01-07 20:53:21 +0530

    calf raises java file created. im so tiredddd

---
+++ java/rest_exr/R_N_calf_raises_legs.java
@@ -0,0 +1,94 @@
+package com.example.fitnessapp.rest_exc;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class R_N_calf_raises_legs extends AppCompatActivity {
+    private int seconds = 15;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.legs_exersices_calf_raises);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Calf Raises");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Stand with your torso upright, your feet hip-width apart, and your toes pointing forward.\n" +
+                "2. Raise your heels off the floor and squeeze your calves.\n" +
+                "3. Return to the starting position, by slowly lowering your heels, and repeat.\n\n"+
+                "When doing calf raises, brace your core, face forward and keep your toes pointing straight ahead. Raise your heels and breathe out as you squeeze your calves. Breathe in as you lower your heels and slowly return to the starting position.");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
