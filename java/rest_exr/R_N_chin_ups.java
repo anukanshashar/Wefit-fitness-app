18962d8c329443edfd0d0e504b767910f4e0d4ba
anukanshashar <75297454+anukanshashar@users.noreply.github.com>
2023-01-07 20:01:24 +0530

    chin ups java file doneeeee

---
+++ java/rest_exr/R_N_chin_ups.java
@@ -0,0 +1,93 @@
+package com.example.fitnessapp.rest_exc;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class R_N_chin_ups extends AppCompatActivity {
+    private int seconds = 15;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.exersice_20_chin_ups);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Chin Ups");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Using a supinated grip, grasp the bar with a shoulder width grip.\n" +
+                "2. Take a deep breath, squeeze your glutes and brace your abs. Depress the shoulder blades and then drive the elbows straight down to the floor while activating the lats.\n" +
+                "3. Pull your chin towards the bar until the lats are fully contracted, then slowly lower yourself back to the start position and repeat for the assigned number of repetitions.");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
