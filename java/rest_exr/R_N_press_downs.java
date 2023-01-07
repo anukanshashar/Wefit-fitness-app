a2b8a75a25dc1e0c2d02ce81c258384e17b0b9c1
anukanshashar <75297454+anukanshashar@users.noreply.github.com>
2023-01-07 21:37:58 +0530

    dumbell leg lifts java file

---
+++ java/rest_exr/R_N_press_downs.java
@@ -0,0 +1,95 @@
+package com.example.fitnessapp.rest_exc;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class R_N_press_downs extends AppCompatActivity {
+    private int seconds = 15;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.exersice_tricep_);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Press Downs");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Standing with a resistance band in your right hand, place your right hand at the front of your left shoulder, right arm against the chest.\n" +
+                "2. Loop the other end of the band around your left hand.\n" +
+                "3. Start with your left arm almost bent to a 90-degree angle with your left elbow tucked close to your body and keep tension in the band.\n" +
+                "4. Press your left hand towards the floor until your left arm is fully extended.\n" +
+                "5. Release to start. Repeat the full number of reps and then switch sides for one set. ");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
