8c3cc6fc0274dea14ffbb53e3e6c72e42b24035d
Ronny <75095886+Ron102@users.noreply.github.com>
2023-01-07 12:54:40 +0530

    bicycle crunches rest exercise done java

---
+++ java/rest_exr/R_N_bicycle_crunches.java
@@ -0,0 +1,95 @@
+package com.example.fitnessapp.rest_exc;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class R_N_bicycle_crunches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.exersice_4);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Bicycle Crunches");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Lie on your back, lift your shoulders off the mat and raise both legs.\n" +
+                "2. Bring one knee and the opposing elbow close to each other by crunching to one side, and fully extend the other leg.\n" +
+                "3. Return to the starting position and then crunch to the opposite side.\n" +
+                "4. Repeat until the set is complete.\n\n" +
+                "When doing bicycle crunches, engage your core muscles,open the elbows and keep yourneck relaxed.Breathe outas you crunch,keep your shoulderlades off and maintain asteady rhythm throughout theentire exercise.");
+        runTimer();
+    }
+    private int seconds = 15;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 15;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
