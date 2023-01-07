840fb51acf1c118facad91d0abf5164087c5cc1b
anukanshashar <75297454+anukanshashar@users.noreply.github.com>
2023-01-07 11:09:58 +0530

    first shoulder exercise

---
+++ java/shoulder_press_first_shoulder.java
@@ -0,0 +1,95 @@
+package com.example.fitnessapp.rest_exc;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class R_N_shoulder_press_first_shoulder extends AppCompatActivity {
+    private int seconds = 15;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.exersice_32);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Shoulder Press Modified");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Lie down on a flat bench and hold a dumbbell in each hand.\n" +
+                "2. Align your elbows in a straight line with shoulders and your elbows under your wrists.\n" +
+                "3. Push the dumbbells up and fully extend your arms.\n" +
+                "4. Lower the dumbbells back down and repeat the movement until the set is complete.\n\n"+
+                "Keep your back straight, open your chest, face forward, and exhale as you push the dumbbells up and over your head. Engage your core muscles to give back support, and maintain your hands shoulder-width apart.");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
