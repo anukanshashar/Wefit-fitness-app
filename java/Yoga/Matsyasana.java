459e4afa0217f5da9ac34fac60018db1e31bde8e
Vaibhav Khandelwal <72210440+humorouswarrior@users.noreply.github.com>
2023-01-08 13:45:27 +0530

    matsyasana javafile

---
+++ java/Yoga/Matsyasana.java
@@ -0,0 +1,90 @@
+package com.example.fitnessapp.yoga;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class Matsyasana extends AppCompatActivity {
+    private int seconds = 35;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.matsyaans);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("Lie on your back and bend your knees while resting your feet on the floor. Inhale while lifting your pelvis slightly off the floor and slide your hands, palms down, below your bottom. Hold for 15 to 30 seconds.");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
