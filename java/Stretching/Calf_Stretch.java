68756704a71ac428582d886ce4863120f4f0092b
Vaibhav Khandelwal <72210440+humorouswarrior@users.noreply.github.com>
2023-01-08 13:36:00 +0530

    calf stretch javafile

---
+++ java/Stretching/Calf_Stretch.java
@@ -0,0 +1,94 @@
+package com.example.fitnessapp.stretching;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class Calf_Stretch extends AppCompatActivity {
+    private int seconds = 35;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.calf_stretch);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Calf Stretch");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Sit down on the floor and bring both feet together.\n" +
+                "2. With the help of your arms, drive your knees down into the floor and bend your head towards your toes as much as its comfortable.\n"
+                +"3. Hold for 5 seconds and then erect your back again repeat the 2nd step again.\n"+
+                "Breath in when going downwards and breath out when coming back erect");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
