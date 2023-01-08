7650807ce7f85cb40e73fcdb311c44b9b795ce34
Vaibhav Khandelwal <72210440+humorouswarrior@users.noreply.github.com>
2023-01-08 13:32:57 +0530

    backleglifts javafile

---
+++ java/start/Start_backleg_lifts.java
@@ -0,0 +1,95 @@
+package com.example.fitnessapp.start;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class Start_Back_Leg_Lifts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.exersice_5);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Leg Raise");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Lie on your back with your hands by your sides or underneath your glutes. Keep your legs straight or just slightly bent.\n" +
+                "2. Raise your legs until your body is in an L shape.\n" +
+                "3. Pause, lower both legs slowly and then return to the starting position.\n" +
+                "4. Repeat until set is complete.\n\n"+"When doing the straight leg raise, keep your abs engaged at all times and keep your back, head, and shoulders pressed against the floor. Breathe in as you lower your legs and, if you feel any discomfort on your lower back, reduce the range of motion by lowering your legs only halfway.");
+        runTimer();
+    }
+    private int seconds = 15;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 15;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+
+}
