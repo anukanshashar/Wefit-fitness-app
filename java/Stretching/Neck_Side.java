5c0dbc8c1edb24881069db24326cf456aa55a07a
anukanshashar <75297454+anukanshashar@users.noreply.github.com>
2023-01-08 13:47:37 +0530

    neck exercises java file

---
+++ java/Stretching/Neck_Side.java
@@ -0,0 +1,95 @@
+package com.example.fitnessapp.stretching;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class Neck_Side extends AppCompatActivity {
+    private int seconds = 30;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stretching_back);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.neck_side_bend);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Neck Side-to-Side");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Stand erect aligning your body in a straight line.\n" +
+                "2. Extend your left hand and rest the palms on the right ear gently.\n" +
+                "3. Gently pull your head to left side until you feel a comfortable stretch in the opposite side of bending.\n" +
+                "4. Hold for 15 seconds and switch.\n\n" +
+                "Breath gently when tilting to one side and breath out coming back to normal position. Do not force yourself to do more.");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
