424d54534762bfad6936c188a2d8eae7721ad9b9
anukanshashar <75297454+anukanshashar@users.noreply.github.com>
2023-01-08 13:29:28 +0530

    stretching exercises java file

---
+++ java/Stretching/Cobra_pose.java
@@ -0,0 +1,94 @@
+package com.example.fitnessapp.stretching;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class Cobra_Pose extends AppCompatActivity {
+    private int seconds = 35;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.cobra_stretch);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Cobra Stretch");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Lie on your belly, place your hands on the floor directly under the shoulders, and keep your elbows close to your torso.\n" +
+                "2. Inhale as you straighten the arms and lift your chest off the floor.\n" +
+                "3. Stay in cobra pose for 15 to 30 seconds.\n\n"+
+                "Root your pelvis, thighs, and tops of the feet to the floor. Be mindful as you extend your arms and distribute the stretch evenly along the spine. Do not fully extend your arms if this feels uncomfortable. Feel your hips narrowing and keep your shoulders and lower back relaxed..");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
---
+++ java/Stretching/Hip_roll.java
@@ -0,0 +1,94 @@
+package com.example.fitnessapp.stretching;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class Hip_Roll extends AppCompatActivity {
+    private int seconds = 35;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifteen_rest);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.hip_roll);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Hip Rotation");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Stand straight with your feet a little wider than shoulder-width apart. Bend the knees slightly and place your hands on the hips.\n"+
+        "2. Slowly rotate your hips, making big circles.\n"+
+        "3. Complete a set in one direction and then switch to the opposite direction.\n\n"+
+        "Keep your body relaxed and face forward, engage your core muscles and breathe deeply maintaining a steady rhythm. Start with small circles and increase their diameter over time, making sure to always keep your hips level with the floor.");
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 45;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+}
