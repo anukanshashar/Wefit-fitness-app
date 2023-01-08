1de3110bf2ef7f41129b319a404d470c1a9f561d
Vaibhav Khandelwal <72210440+humorouswarrior@users.noreply.github.com>
2023-01-08 13:34:11 +0530

    legdumbelllift javafile

---
+++ java/start/Start_leg_dumbells_deadlift.java
@@ -0,0 +1,95 @@
+package com.example.fitnessapp.start;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.R;
+
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class Start_Legs_Dumbell_DeadLift extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+        GifImageView gif = (GifImageView) findViewById(R.id.exc);
+        gif.setBackgroundResource(R.drawable.legs_exercises_dumbell_deadlift);
+        TextView name =  (TextView) findViewById(R.id.exc_name);
+        name.setText("Dumbell Dead Lifts");
+        TextView instruction =  (TextView) findViewById(R.id.instructions);
+        instruction.setText("1. Pick up a pair of dumbbells with an overhand grip and hold them in front of your sides. Stand with your knees slightly bent, and your feet placed shoulder-width apart. \n" +
+                "2. Bend at the hips and knees, lowering your torso until it’s almost parallel with the floor. \n" +
+                "3. Allow your arms to hang down in front of your knees and shins. Make sure you keep your back in a neutral position, taking care not to round it. You should lower yourself into position slowly, in a controlled manner. \n" +
+                "4. From this position, stand up straight without changing the shape of your back. Famatumi recommends squeezing your glutes as you straight, pushing through the ball and heel of your foot. That’s one rep.  ");
+        runTimer();
+    }
+    private int seconds = 15;
+    private boolean running = true;
+    private boolean wasRunning;
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 15;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.timer);
+        final Handler handler = new Handler();
+
+        // handler is used to send and process message and runnable objects associated with threads queue
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%02d", secs);
+                timeView.setText(time);
+                if (running && seconds >= 1) {
+                    seconds--;
+                }
+                if(seconds==0){
+                    finish();
+                }
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
+    }
+
+}
